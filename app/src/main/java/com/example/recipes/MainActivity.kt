e87a3cc46d5befec43cf5064e187d965ba2a481d
lightbringer2 <jnbalcarek@gmail.com>
2024-02-12 17:50:30 +0100

    MainActivity - rozpracováno
    Dodělat ovládací tlačítka, přidat MVVM.

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnClose: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        btnClose = findViewById(R.id.btnClose)
+
+        btnClose.setOnClickListener {
+            finishAffinity()
+        }
     }
 }
