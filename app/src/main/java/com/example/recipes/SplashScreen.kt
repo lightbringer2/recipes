9cf5745d18978800077a93ba1cb08d44307b8ee7
lightbringer2 <jnbalcarek@gmail.com>
2024-02-12 15:52:50 +0100

    Splash screen, ikonka, main activity
    SplashScreen přidáno, ikonka, přidáno, main activity - prázdné přidáno.

---
+++ app/src/main/java/com/example/recipes/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.recipes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 5000)
+    }
+}
