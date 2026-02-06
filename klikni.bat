a286da0acb1f01cafdab1d4fe5490859c8ebbf6f
Jan Balcárek <jnbalcarek@gmail.com>
2026-02-06 12:32:24 +0100

    Enhance klikni.bat to log user and system info
    The script now collects user and system information and saves it to 'info.txt'.

--- klikni.bat
+++ klikni.bat
@@ -1,2 +1,24 @@
-rem kliknul
-echo "kliknul"
+@echo off
+chcp 65001 >nul
+
+set OUTPUT=info.txt
+
+echo ===== Informace o uživateli ===== > %OUTPUT%
+echo Datum a čas: %DATE% %TIME% >> %OUTPUT%
+echo. >> %OUTPUT%
+
+echo Uživatelské jméno: %USERNAME% >> %OUTPUT%
+echo Doména / PC: %USERDOMAIN% >> %OUTPUT%
+echo Domovská složka: %USERPROFILE% >> %OUTPUT%
+echo. >> %OUTPUT%
+
+echo ===== Informace o systému ===== >> %OUTPUT%
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Type" >> %OUTPUT%
+echo. >> %OUTPUT%
+
+echo ===== Síť ===== >> %OUTPUT%
+ipconfig | findstr /C:"IPv4" >> %OUTPUT%
+
+echo. >> %OUTPUT%
+echo Hotovo. Informace uloženy do %OUTPUT%.
+pause
